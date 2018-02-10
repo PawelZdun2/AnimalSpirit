6c59a76549e5c63506e47ceca371405b3ac208b0
brsk88 <borsewicz.michal@gmail.com>
2018-02-10 23:09:04 +0100

    add api

--- AnimalSpiritAPI/Controllers/ValuesController.cs
+++ AnimalSpiritAPI/Controllers/ValuesController.cs
@@ -7,35 +7,35 @@ using Microsoft.AspNetCore.Mvc;
 namespace AnimalSpiritAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class AnimalSpiritController : Controller
     {
-        // GET api/values
+        // GET api/AnimalSpirit
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/AnimalSpirit/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/AnimalSpirit
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/AnimalSpirit/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/AnimalSpirit/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
